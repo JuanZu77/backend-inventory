9e3a3b321e817c8cc355f3ca2c59f4502d450a9a
JuanZu77 <juanvzubiri77@gmail.com>
2024-04-17 20:58:25 -0300

    guardar productos

---
+++ src/main/java/com/company/inventory/services/ProductServiceImpl.java
@@ -0,0 +1,81 @@
+package com.company.inventory.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.company.inventory.dao.ICategoryDao;
+import com.company.inventory.dao.IProductDao;
+import com.company.inventory.model.Category;
+import com.company.inventory.model.Product;
+import com.company.inventory.response.ProductResponseRest;
+
+@Service
+public class ProductServiceImpl implements IProductService {
+	
+	//INYECTAR DEPENDENCIAS con @Autowired
+	//@Autowired
+	//private ICategoryDao categoryDao;
+	
+	
+	//Inyectar Dependencias con CONSTRUCTOR
+	private ICategoryDao categoryDao;
+	private IProductDao productDao;
+	
+	public ProductServiceImpl (ICategoryDao categoryDao, IProductDao productDao) {
+		
+		super();
+		this.categoryDao = categoryDao;
+		this.productDao = productDao;
+	}
+		
+	
+	@Override
+	public ResponseEntity<ProductResponseRest> save(Product product, Long categoryId) {
+	
+		ProductResponseRest response = new ProductResponseRest();
+		List<Product> list = new ArrayList<>();
+		
+		try {
+			//Search Category to set in the product
+			Optional<Category> category = categoryDao.findById(categoryId);
+			
+			if(category.isPresent()) {
+				product.setCategory(category.get());
+			}else {
+				response.setMetadata("Error", "-1", "Categoria No encontrada");
+				return new ResponseEntity<ProductResponseRest>(response, HttpStatus.NOT_FOUND);
+			}
+			
+			//Save Product
+			Product productSaved =  productDao.save(product);
+			
+			if(productSaved != null) {
+				list.add(productSaved);
+				response.getProductResponse().setProducts(list);
+				response.setMetadata("Respuesta OK", "00", "Producto Guardado");
+			}else {
+				response.setMetadata("Error", "-1", "Error al Guardar Producto");
+				return new ResponseEntity<ProductResponseRest>(response, HttpStatus.BAD_REQUEST);
+			}
+			
+		} 
+		catch(Exception e){
+			e.getStackTrace(); //más info del error
+			response.setMetadata("Error", "-1", "Error al consultar");
+			return new ResponseEntity<ProductResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		return new ResponseEntity<ProductResponseRest>(response, HttpStatus.OK);
+
+	};
+
+	
+	
+	
+	
+};
